501ce8af237f2906671902bedadc32bcf7d7da57
Moses Oyet David <trickmaxillar@gmail.com>
2022-01-30 18:52:47 +0300

    Modify

--- models/base_model.py
+++ models/base_model.py
@@ -7,25 +7,11 @@ for other classes
 import uuid
 from datetime import datetime
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 class BaseModel:
     """Class for base model"""
     def __init__(self, *args, **kwargs):
         """Initializing of instances"""
-        if kwargs is not None and kwargs != {}:
-            for key, value in kwargs.items():
-                if key not in '__class__':
-                    setattr(self, key, value)
-                if key == 'created_at':
-                    self.__dic__['created_at'] = datetime.strptime(
-                            kwargs['created_at'], time)
-                elif key == 'updated_at':
-                    self.__dic__['updated_at'] = datetime.strptime(
-                            kwargs['updated_at'], time)
-                else:
-                    self.__dic__[key] = kwargs[key]
-        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
