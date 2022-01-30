fe8469e8bac0481af51c0bd3ee778c2e7c2d74ac
Moses Oyet David <trickmaxillar@gmail.com>
2022-01-30 17:45:48 +0300

    Modify

--- models/base_model.py
+++ models/base_model.py
@@ -12,32 +12,17 @@ class BaseModel:
     """Defines the common attributes/methods for other classes
     """
 
-    def __init__(self, *args, **kwargs):
-        """Initializes a new instance of BaseModel class
-        Args:
-            *args (any type): A variable number of arguments
-            **kwargs (dict): Key/value pairs of attributes
-        """
+    def __init__(self):
+        """Initializes a new instance of BaseModel classArgs:"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    t_format = "%Y-%m-%dT%H:%M:%S.%f"
-                    self.__dict__[key] = datetime.strptime(value, t_format)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def save(self):
         """Updates the public instance attribute `updated_at`
         with the current datetime
         """
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values
@@ -53,4 +38,4 @@ class BaseModel:
         """Returns the string representation of an instance of BaseModel
         """
         clsname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
+        return "{[:s]} {(:s)} {}".format(clsname, self.id, self.__dict__)
