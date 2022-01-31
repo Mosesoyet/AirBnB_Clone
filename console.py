12b76e75ae8c7b0e0bd8cbe9be515b101f72285e
Moses Oyet David <trickmaxillar@gmail.com>
2022-01-31 03:24:22 +0300

    Modify

--- console.py
+++ console.py
@@ -6,6 +6,22 @@ a class that contain the console command
 import cmd
 from datetime import datetime
 
-class HBNBCommand(cmd.cmd):
+class HBNBCommand(cmd.Cmd):
     """The HBNB console"""
-    
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """implements End Of File"""
+        print()
+        return True
+
+    def do_quite(self, line):
+        """Exits the program."""
+        return True
+
+    def emptyline(self):
+        """Does nothing on ENTER"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
