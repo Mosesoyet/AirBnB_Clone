4346dc42efc03553837acb2a9a8d376b43c0f88c
Moses Oyet David <trickmaxillar@gmail.com>
2022-02-05 21:00:06 +0300

    Modify

--- console.py
+++ console.py
@@ -5,23 +5,24 @@ a class that contain the console command
 
 import cmd
 from datetime import datetime
+import shlex #For splitting the line along spaces except in double quotes
 
 class HBNBCommand(cmd.Cmd):
     """The HBNB console"""
     prompt = '(hbnb)'
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """implements End Of File"""
         print()
         return True
 
-    def do_quite(self, line):
+    def do_quit(self, arg):
         """Exits the program."""
         return True
 
     def emptyline(self):
         """Does nothing on ENTER"""
-        pass
+        return False
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
